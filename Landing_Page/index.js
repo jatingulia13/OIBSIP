2e59252b4f4defbf3d8bb36da693dfd61bb070bf
jatingulia13 <130473688+jatingulia13@users.noreply.github.com>
2024-07-13 12:37:28 +0530

    Files added

---
+++ Landing_Page/index.js
@@ -0,0 +1,32 @@
+// console.log("enter any number :");
+// let a = prompt("enter a :");
+// let b  = prompt("enter b :"); 
+// let operation ;
+// let operation1 =prompt("enter operation:"); 
+// let opp = {
+//     "+" : "-",
+//     "-" : "+",
+//     "*" : "/",
+//     "/" : "*",
+    
+// }
+// operation = Math.random() ;
+// console.log(operation);
+// if(operation <0.5){
+//     operation1 = opp[operation1];
+//     alert(`the answer is ${eval(`${a} ${operation1} ${b}`)}`)
+    
+// }
+// else{
+//     alert(`the answer is ${eval(`${a} ${operation1} ${b}`)}`)
+// }
+
+
+let array ;
+array[10]=null
+for(let i = 0 ; i < 10 ; i++){
+    array[i] = prompt();
+}
+for(let i = 0 ; i < 10 ; i++){
+    console.log(array[i])
+}
